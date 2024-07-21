d232fa2abd082a093610658e19a759b54cf4cc9a
riparraguirre <riparraguirre@marketaxess.com>
2024-07-21 12:38:52 -0300

    greetings launcher

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/launcher/GreetingsStreamApp.java
@@ -1,8 +1,13 @@
 package com.learnkafkastreams.launcher;
 
+import com.learnkafkastreams.topology.GreetingsTopology;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 
 import java.util.List;
 import java.util.Properties;
@@ -10,19 +15,35 @@ import java.util.stream.Collectors;
 
 @Slf4j
 public class GreetingsStreamApp {
-
     public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "greetings-stream-app");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
 
+        createTopics(props, List.of(GreetingsTopology.SOURCE_TOPIC, GreetingsTopology.DESTINATION_TOPIC));
 
+        Topology greetingsTopology = GreetingsTopology.buildTopology();
+
+        KafkaStreams kafkaStreams = new KafkaStreams(greetingsTopology, props);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(kafkaStreams::close));
+
+        try {
+            kafkaStreams.start();
+        }
+        catch (Exception e) {
+            log.error("Exception starting kafka stream",e);
+        }
     }
 
-    private static void createTopics(Properties config, List<String> greetings) {
+    private static void createTopics(Properties config, List<String> topicsToBeCreated) {
 
         AdminClient admin = AdminClient.create(config);
         var partitions = 1;
         short replication  = 1;
 
-        var newTopics = greetings
+        var newTopics = topicsToBeCreated
                 .stream()
                 .map(topic ->{
                     return new NewTopic(topic, partitions, replication);
--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -5,20 +5,25 @@ import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 import org.apache.kafka.streams.kstream.Produced;
 
 public class GreetingsTopology {
-    private static String SOURCE_TOPIC = "greetings";
-    private static String DESTINATION_TOPIC = "greetings_uppercase";
+    public static String SOURCE_TOPIC = "greetings";
+    public static String DESTINATION_TOPIC = "greetings_uppercase";
 
     public static Topology buildTopology() {
         final StreamsBuilder streamsBuilder = new StreamsBuilder();
 
         KStream<String, String> greetingsStream = streamsBuilder.stream(SOURCE_TOPIC, Consumed.with(Serdes.String(), Serdes.String()));
 
+        greetingsStream.print(Printed.<String,String>toSysOut().withLabel("greetingsStream"));
+
         KStream<String, String> modifiedStream = greetingsStream.mapValues(((readOnlyKey, value) -> value.toUpperCase() ));
 
         modifiedStream.to(DESTINATION_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+
+        modifiedStream.print(Printed.<String,String>toSysOut().withLabel("modifiedGreetingsStream"));
 
         return streamsBuilder.build();
     }
