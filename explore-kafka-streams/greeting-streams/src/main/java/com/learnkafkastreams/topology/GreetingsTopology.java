9c58887671219d30352195afbdab29642f254d1f
riparraguirre <riparraguirre@marketaxess.com>
2024-07-21 12:18:12 -0300

    greetings topology

--- explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
+++ explore-kafka-streams/greeting-streams/src/main/java/com/learnkafkastreams/topology/GreetingsTopology.java
@@ -1,4 +1,35 @@
 package com.learnkafkastreams.topology;
 
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
+
 public class GreetingsTopology {
+    private static String SOURCE_TOPIC = "greetings";
+    private static String DESTINATION_TOPIC = "greetings_uppercase";
+
+    public static Topology buildTopology() {
+        final StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        KStream<String, String> greetingsStream = streamsBuilder.stream(SOURCE_TOPIC, Consumed.with(Serdes.String(), Serdes.String()));
+
+        KStream<String, String> modifiedStream = greetingsStream.mapValues(((readOnlyKey, value) -> value.toUpperCase() ));
+
+        modifiedStream.to(DESTINATION_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+
+        return streamsBuilder.build();
+    }
+
+    public static Topology buildTopologyRoWay() {
+        final StreamsBuilder streamsBuilder = new StreamsBuilder();
+
+        streamsBuilder.stream(SOURCE_TOPIC, Consumed.with(Serdes.String(), Serdes.String()))
+                      .mapValues(((readOnlyKey, value) -> value.toUpperCase() ))
+                      .to(DESTINATION_TOPIC, Produced.with(Serdes.String(), Serdes.String()));
+
+        return streamsBuilder.build();
+    }
 }
